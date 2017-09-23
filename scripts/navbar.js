8d690ca933617eded71dfee85e9b93abd5c8bdcb
piotrkramarz <piotr.m.kramarz@gmail.com>
2017-09-23 20:41:45 +0200

    Add shrink navbar, change color
    minor color/visual changes (button etc.)

---
+++ scripts/navbar.js
@@ -0,0 +1,9 @@
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('nav').addClass('shrink');
+        $('.add').hide();
+    } else {
+        $('nav').removeClass('shrink');
+        $('.add').show();
+    }
+});
